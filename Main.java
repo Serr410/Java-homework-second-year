6f866ad151bdd1245d757b48c5c935539d10e002
Артур Завьялов <135697812+Serr410@users.noreply.github.com>
2025-10-12 23:17:49 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -111,6 +111,68 @@ public class Main {
 
 
             }
+            case "2" -> {
+                Lab2.Human person1 = new Lab2.Human("Клеопатра", "", "", 152);
+                Lab2.Human person2 = new Lab2.Human("Александр", "Пушкин", "Сергеевич", 167);
+                Lab2.Human person3 = new Lab2.Human("Владимир", "Маяковский", "", 189);
+                Lab2.Human person10 = new Lab2.Human("Христофор", "Банифатьевич", "", 100);
+
+                System.out.println("Current human beings:");
+                System.out.println(person1.toString());
+                System.out.println(person2.toString());
+                System.out.println(person3.toString());
+                System.out.println(person10.toString() + " (height used for the sake of simplicity)" + "\n");
+
+                Lab2.Human person4 = new Lab2.Human("Иван", "Чудов", "", 160);
+                Lab2.Human person5 = new Lab2.Human("Петр", 156, person4);
+                Lab2.Human person6 = new Lab2.Human("Борис", 186, person5);
+                System.out.println("Like father like son:");
+                System.out.println(person4.toString());
+                System.out.println(person5.toString());
+                System.out.println(person6.toString() + "\n");
+
+                Lab2.Human person7 = new Lab2.Human("Лев", 170);
+                Lab2.Human person8 = new Lab2.Human("Сергей", "Пушкин", 168, person7);
+                Lab2.Human person9 = new Lab2.Human("Александр", 167, person8);
+                System.out.println("Russian roulette of generation:");
+                System.out.println(person7.toString() + "    Name and height");
+                System.out.println(person8.toString() + "    Name, Surname, height and father");
+                System.out.println(person9.toString() + "    Name, height and father" + "\n");
+
+                Lab2.Cat Kitty = new Lab2.Cat("Барсик");
+                System.out.println(Kitty.toString());
+                Kitty.Meow();
+                Kitty.Meow(3);
+                System.out.println("\n");
+
+                Lab2.City A = new Lab2.City("A");
+                Lab2.City B = new Lab2.City("B");
+                Lab2.City C = new Lab2.City("C");
+                Lab2.City D = new Lab2.City("D");
+                Lab2.City E = new Lab2.City("E");
+                Lab2.City F = new Lab2.City("F");
+
+                A.addRoute(F, 1);
+                A.addRoute(B, 5);
+                A.addRoute(D, 6);
+                B.addRoute(A, 5);
+                B.addRoute(C, 3);
+                C.addRoute(B, 3);
+                C.addRoute(D, 4);
+                D.addRoute(C, 4);
+                D.addRoute(E, 2);
+                D.addRoute(A, 6);
+                E.addRoute(F, 2);
+                F.addRoute(E, 2);
+                F.addRoute(B, 1);
+
+                System.out.println(A.getCityData());
+                System.out.println(B.getCityData());
+                System.out.println(C.getCityData());
+                System.out.println(D.getCityData());
+                System.out.println(E.getCityData());
+                System.out.println(F.getCityData());
+            }
             default -> System.out.println("Incorrect input, this lab doesn't exist yet. " +
                     "try again be restarting the program.");
         }
