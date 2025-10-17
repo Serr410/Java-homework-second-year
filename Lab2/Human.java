0eb0a9265507eed39b546671f29b443c17476b5c
Артур Завьялов <135697812+Serr410@users.noreply.github.com>
2025-10-17 16:30:52 +0500

    Update Human.java

--- Lab2/Human.java
+++ Lab2/Human.java
@@ -1,1 +1,49 @@
+package Lab2;
+
+public class Human {
+    private int height;
+    private Name name;
+    private Human father;
+    public void setHeight(int height) {
+        if (height < 50) {
+            System.out.println("Error. Height impossible in " + this.name.toString());
+        } else {
+            this.height = height;
+        }
+    }
+
+    public Human(String Fname, int height, Human father){
+        this.name = new Name(Fname, father.name.Sname, father.name.Fname + "ович");
+        this.setHeight((height));
+        this.father = father;
+    }
+    public Human(String Fname, String Sname, String Lname, int height){
+        this.name = new Name(Fname, Sname, Lname);
+        this.setHeight((height));
+        this.father = new Human("Null-human");
+    }
+    private Human(String Fname){
+        this.name = new Name(Fname, "", "");
+        this.height = -1;
+    }
+    public Human(){
+        this.name = new Name();
+        this.height = -1;
+        this.father = new Human("Null-human");
+    }
+    public Human(String Fname, int height){
+        this.name = new Name(Fname, "", "");
+        this.setHeight(height);
+    }
+    public Human(String Fname, String Sname, int height, Human father){
+        this.name = new Name(Fname, Sname, father.name.Fname + "ович");
+        this.setHeight((height));
+        this.father = father;
+    }
+
+    @Override
+    public String toString() {
+        return (name.toString() + " " + height);
+    }
+}
 
